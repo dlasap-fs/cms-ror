84a24e90814659e87ddda9c64741134eb50e0b35
dlasap-fs <dlasap@fullscale.io>
2023-05-02 21:49:25 +0800

    added filtering for contacts and explored around

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,20 @@ class HomeController < ApplicationController
   end
 
   def about
+    @about_me = "My name is John Doe..."
+    @answer = 6 + 4
+
+    #get a result from an api basic
+    require 'net/http'
+    require 'json'
+
+    @url = 'https://api.coinmarketcap.com/v1/ticker/'
+    @uri = URI(@url)
+    @response = Net::HTTP.get(@uri)
+    # puts @response
+
+
+
+
   end
 end
